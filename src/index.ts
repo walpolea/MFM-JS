dfd9e99d1089b77f5f814c764a589bd407879150
Andrew Walpole <walpolea@gmail.com>
2018-10-25 14:16:46 -0700

    click to add dreg visual changes

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,20 @@ import { Site } from "./mfm/classes/Site";
 import { ElementTypes } from "./mfm/classes/ElementTypes";
 import { MFMUtils } from "./mfm/utils/utils";
 import { DRegElement } from "./mfm/classes/elements/DRegElement";
+import { Atom } from "./mfm/classes/Atom";
 
 declare var p5: any;
 
-let g: Tile = new Tile(30, 30);
-
-console.log(g);
-console.log(g.getRandomSite());
-g.getRandomSite().atom.elem = new DRegElement();
-g.getRandomSite().atom.elem = new DRegElement();
-g.getRandomSite().atom.elem = new DRegElement();
-g.getRandomSite().atom.elem = new DRegElement();
+let g: Tile = new Tile(32, 32);
 
 var sketch = (p: any) => {
-  let siteSize = 12;
+  let siteSize = 14;
+  let gridOffset = 25;
 
   p.preload = () => {};
 
   p.setup = () => {
-    p.createCanvas(600, 600);
+    p.createCanvas(700, 700);
   };
 
   //   p.windowResized = () => {
@@ -30,20 +25,23 @@ var sketch = (p: any) => {
 
   let drawGrid = (p: any, t: Tile) => {
     p.push();
-    p.translate(50, 50);
+    p.translate(gridOffset, gridOffset);
     t.sites.forEach((site: Site) => {
       switch (site.atom.elem.type) {
         case ElementTypes.EMPTY.type:
-          p.fill(32);
+          p.stroke(0, 0, 0, 0);
+          p.fill(32, 32, 32, 127);
           break;
         case ElementTypes.DREG.type:
+          p.stroke(0, 0, 0, 127);
           p.fill(255, 32, 32);
           break;
         case ElementTypes.RES.type:
+          p.stroke(0, 0, 0, 127);
           p.fill(32, 255, 64);
           break;
       }
-      p.stroke(0);
+
       p.ellipse(site.tilePos.col * siteSize, site.tilePos.row * siteSize, siteSize, siteSize);
     });
     p.pop();
@@ -57,11 +55,26 @@ var sketch = (p: any) => {
     }
   };
 
+  let getSiteFromCanvasXY = (x: number, y: number): Site => {
+    x = x - gridOffset + siteSize * 0.5;
+    y = y - gridOffset + siteSize * 0.5;
+
+    x = Math.floor(x / siteSize);
+    y = Math.floor(y / siteSize);
+
+    return g.getSiteByCoord({ row: y, col: x });
+  };
+
   p.draw = () => {
     p.background(100);
     drawGrid(p, g);
 
     run();
+  };
+
+  p.mouseClicked = () => {
+    let site: Site = getSiteFromCanvasXY(p.mouseX, p.mouseY);
+    if (site) site.atom = new Atom(ElementTypes.DREG);
   };
 };
 
