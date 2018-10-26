6060d5af31849370f19e793c686d3dddb95a1edd
Dave Ackley <ackley@ackleyshack.com>
2018-10-26 06:21:16 -0600

    DReg can degrade Wall
    ..In the standard view of things, DReg is extremely powerful,
    precisely to force anything that co-exists with it to be
    actively robust enough to handle DReg's effects.  Wall by
    itself isn't that.  (Wall + Mason or other Wall builder,
    perhaps.)

--- src/mfm/classes/elements/WallElement.ts
+++ src/mfm/classes/elements/WallElement.ts
@@ -4,7 +4,7 @@ import { ElementTypes } from "../ElementTypes";
 
 export class WallElement extends Elem {
   constructor() {
-    super(ElementTypes.WALL.name, ElementTypes.WALL.type, 0, 0);
+    super(ElementTypes.WALL.name, ElementTypes.WALL.type, 0, 100);
   }
   exec(ew: EventWindow) {
     super.exec(ew);
