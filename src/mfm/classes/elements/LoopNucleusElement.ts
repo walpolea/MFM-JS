42d8ce03d167227c1820f98d13139f620a9dd54e
Andrew Walpole <walpolea@gmail.com>
2019-04-05 22:10:01 -0700

    rebalancing, thicker membrane for loop

--- src/mfm/classes/elements/LoopNucleusElement.ts
+++ src/mfm/classes/elements/LoopNucleusElement.ts
@@ -7,9 +7,10 @@ import { Site } from "../Site";
 
 export class LoopNucleusElement extends Elem {
 
-  pCREATE_RES: number = 100;
-  pCREATE_MEMBRANE: number = 10;
+  pCREATE_RES: number = 20;
+  pCREATE_MEMBRANE: number = 2;
   reproductionConditionCount: number = 0;
+  aloneCount: number = 0;
 
   constructor() {
     super(ElementTypes.LOOPNUCLEUS.name, ElementTypes.LOOPNUCLEUS.type);
@@ -40,13 +41,25 @@ export class LoopNucleusElement extends Elem {
 
   exec(ew: EventWindow) {
 
+    if (ew.getAll(ElementTypes.LOOPWORM).length === 0) {
+      this.aloneCount++;
+    } else {
+      this.aloneCount = 0;
+    }
+
+    //if the loop dies the nucleus should die at some point, also if the loop gets too big
+    if (this.aloneCount > 100) {
+      ew.origin.killSelf();
+    }
+
+
     if (MFMUtils.oneIn(this.pCREATE_RES)) {
       ew.origin.mutateSite(ew.getAdjacent4Way(ElementTypes.EMPTY), new Atom(ElementTypes.RES));
     }
 
     //create a new membrane if none are around and chance
-    if (!ew.getAdjacent8Way(ElementTypes.STICKYMEMBRANE) && MFMUtils.oneIn(this.pCREATE_MEMBRANE)) {
-      ew.origin.mutateSite(ew.getAdjacent4Way(ElementTypes.EMPTY), new Atom(ElementTypes.STICKYMEMBRANE, [ElementTypes.LOOPWORM]));
+    if (MFMUtils.oneIn(this.pCREATE_MEMBRANE)) {
+      ew.origin.mutateSite(ew.getAdjacent4Way(ElementTypes.EMPTY), new Atom(ElementTypes.STICKYMEMBRANE, [ElementTypes.LOOPWORM, 0.65, 1]));
     }
 
     ew.origin.swapAtoms(ew.getAdjacent4Way(ElementTypes.EMPTY));
