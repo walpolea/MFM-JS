e0720729d26f3e83c4c912c179486c44c59dfa4e
Andrew Walpole <walpolea@gmail.com>
2019-10-12 07:26:39 -0700

    Clean up Sand

--- src/mfm/classes/elements/SandElement.ts
+++ src/mfm/classes/elements/SandElement.ts
@@ -11,30 +11,6 @@ export class Sand extends Elem {
 	static TYPE_DEF: IElementType = { name: "SAND", type: "SB", class: Sand, color: 0xffdd00 };
 	static CREATE = Sand.CREATOR();
 
-	static checkDownEmpty = SPLAT.splatToMap(`
-		~
-		@
-		_
-	`)
-
-	static checkDownWater = SPLAT.splatToMap(`
-		~
-		@
-		W
-	`)
-
-	static checkSlideEmpty = SPLAT.splatToMap(`
-		~~~
-		~@_
-		~~_
-	`);
-
-	static checkSlideWater = SPLAT.splatToMap(`
-		~~~
-		~@W
-		~~W
-	`);
-
 	static checkDownEW = SPLAT.splatToMap(`
 		~
 		@
@@ -70,39 +46,6 @@ export class Sand extends Elem {
 	exec(ew: EventWindow) {
 
 		//Using SPLAT
-
-		//Should I fall?
-		// const fallResult = ew.query(Sand.checkDownEmpty, 0, Sand.SPLAT_MAP);
-		// if (fallResult) {
-		// 	const emptySite = fallResult.get(Empty.TYPE_DEF)[0];
-		// 	ew.swap(emptySite);
-		// 	return;
-		// }
-
-		// //Should I displace?
-		// const displaceResult = ew.query(Sand.checkDownWater, 0, Sand.SPLAT_MAP);
-		// if (displaceResult) {
-		// 	const waterSite = displaceResult.get(Water.TYPE_DEF)[0];
-		// 	ew.swap(waterSite);
-		// 	return;
-		// }
-
-		// //Should I slide?
-		// const slideEmptyResult = ew.query(Sand.checkSlideEmpty, 0, Sand.SPLAT_MAP, Symmetries.REFLECTX);
-		// if (slideEmptyResult) {
-		// 	const emptySite = slideEmptyResult.get(Empty.TYPE_DEF)[0];
-		// 	ew.swap(emptySite);
-		// 	return;
-		// }
-
-		// //Should I slide in water?
-		// const slideWaterResult = ew.query(Sand.checkSlideWater, 0, Sand.SPLAT_MAP, Symmetries.REFLECTX);
-		// if (slideWaterResult) {
-		// 	const waterSite = slideWaterResult.get(Water.TYPE_DEF)[0];
-		// 	ew.swap(waterSite);
-		// 	return;
-		// }
-
 		const fallResult = ew.query(Sand.checkDownEW, 0, Sand.SPLAT_MAP);
 		if (fallResult) {
 			const site = fallResult.get(Empty.TYPE_DEF) ? fallResult.get(Empty.TYPE_DEF)[0] : fallResult.get(Water.TYPE_DEF)[0];
