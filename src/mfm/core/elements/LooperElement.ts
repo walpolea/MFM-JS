dfda4e873045d8a2e13e00af93fdbcd20965b9fd
Andrew Walpole <andrewwalpole@Andrews-MacBook-Pro.local>
2020-07-19 11:31:17 -0700

    looper builds walls

--- src/mfm/core/elements/LooperElement.ts
+++ src/mfm/core/elements/LooperElement.ts
@@ -9,6 +9,7 @@ import { Utils } from "../../utils/MFMUtils";
 import { Atom } from "../Atom";
 import { Wayfinder, Direction } from "../../utils/MFMWayfinder";
 import { DecayWall } from "./DecayWallElement";
+import { Wall } from "./WallElement";
 
 export class Looper extends Elem {
   static TYPE_DEF: IElementType = { name: "Looper", type: "Lo", class: Looper, color: 0xaaaaff };
@@ -24,9 +25,9 @@ export class Looper extends Elem {
 
   constructor(_direction: Direction = "S") {
     super(Looper.TYPE_DEF);
-    this.direction = Wayfinder.DIRECTIONS[(Wayfinder.DIRECTIONS.length * Math.random()) >> 0];
+    this.direction = Wayfinder.DIRECTIONS[(Wayfinder.DIRECTIONS_PRIMARY.length * Math.random()) >> 0];
 
-    this.max = (2 + Math.random() * 4) >> 0;
+    this.max = (4 + Math.random() * 4) >> 0;
   }
 
   reverse() {
@@ -37,8 +38,18 @@ export class Looper extends Elem {
     this.counter++;
     const travelTo: EWIndex = Wayfinder.getDirectionalMove(this.direction, true);
 
-    if (ew.is(travelTo, Empty.TYPE_DEF)) {
-      ew.move(travelTo, DecayWall.CREATE([10]));
+    const makeWall: EWIndex = Wayfinder.getDirectionalMove(Wayfinder.turnLeft(this.direction), true);
+    if (ew.is(makeWall, Empty.TYPE_DEF)) {
+      ew.mutate(makeWall, DecayWall.CREATE([150]));
+    }
+
+    const makeWall2: EWIndex = Wayfinder.getDirectionalMove(Wayfinder.veerLeft(this.direction), true);
+    if (ew.is(makeWall2, Empty.TYPE_DEF)) {
+      ew.mutate(makeWall2, DecayWall.CREATE([150]));
+    }
+
+    if (ew.is(travelTo, [Empty.TYPE_DEF, DecayWall.TYPE_DEF])) {
+      ew.move(travelTo, DecayWall.CREATE([150]));
 
       console.log(this.direction);
     } else {
