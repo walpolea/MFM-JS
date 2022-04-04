6f4ef013792103466d2f847a6404625adf307c50
Andrew Walpole <walpolea@gmail.com>
2022-04-04 12:56:00 -0700

    antiforkbomb

---
+++ src/elements/agents/Sentry.ts
@@ -0,0 +1,36 @@
+import { Repel } from "../../capabilities/Repel";
+import { Wayfinding } from "../../capabilities/Wayfinding";
+import { Element, IElementType } from "../../mfm/Element";
+import { EventWindow } from "../../mfm/EventWindow";
+import { Wayfinder } from "../../mfm/Wayfinder";
+import { AntiForkBomb } from "../core/AntiForkBomb";
+import { Wall } from "../core/Wall";
+
+export class Sentry extends Element {
+  //VARIANTS
+  static CREATE = Sentry.CREATOR({ name: "SENTRY", class: Sentry, color: 0xa414ff, classifications: ["DIRECTIONAL", "DIRECTABLE"], groups: ["MFM"] });
+
+  constructor(type: IElementType, state: any = {}) {
+    super(type, state);
+    this.init();
+  }
+
+  init() {}
+
+  behave(ew: EventWindow) {
+    super.behave(ew);
+
+    if (!this.state.heading) {
+      Wayfinding.SET_DIRECTION(this, Wayfinder.RANDOM());
+    } else {
+      Wayfinding.MOVE_DIRECTIONALLY(ew, this);
+      Wayfinding.SLIGHT_RANDOMLY(this);
+    }
+
+    const forkbombs = ew.filter(EventWindow.ALLADJACENT, "FORKBOMB", true);
+
+    if (forkbombs.length) {
+      ew.mutate(forkbombs[0], AntiForkBomb.CREATE); // EventWindow.RANDOM(EventWindow.ADJACENT4WAY), AntiForkBomb.CREATE({ color: (this.rd("color") + 512) % 0xffffff }));
+    }
+  }
+}
---
+++ src/elements/core/AntiForkBomb.ts
@@ -0,0 +1,23 @@
+import { Decay } from "../../capabilities/Decay";
+import { Element, IElementType } from "../../mfm/Element";
+import { EventWindow } from "../../mfm/EventWindow";
+
+export class AntiForkBomb extends Element {
+  static CREATE = AntiForkBomb.CREATOR({ name: "ANTIFORKBOMB", symbol: "AFK", class: AntiForkBomb, color: 0x00dddd, groups: ["MFM"] });
+
+  constructor(type: IElementType, state: any = {}) {
+    super(type, state);
+  }
+
+  behave(ew: EventWindow) {
+    super.behave(ew);
+
+    const forkbombs = ew.filterByType(EventWindow.ADJACENT8WAY, "FORKBOMB");
+
+    if (forkbombs.length) {
+      ew.mutateMany(forkbombs, AntiForkBomb.CREATE, [{ color: (this.rd("color") + 2048) % 0xffffff }, {}]); // EventWindow.RANDOM(EventWindow.ADJACENT4WAY), AntiForkBomb.CREATE({ color: (this.rd("color") + 512) % 0xffffff }));
+    }
+
+    Decay.DECAY(ew, this, 10);
+  }
+}
